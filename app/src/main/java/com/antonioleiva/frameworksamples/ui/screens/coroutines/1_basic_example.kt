e8736b8bbc69af83d9d7575a42887d8674ea2a1a
antoniolg <antonio@devexperto.com>
2025-03-18 21:20:12 +0100

    Coroutines:  basic example

---
+++ app/src/main/java/com/antonioleiva/frameworksamples/ui/screens/coroutines/1_basic_example.kt
@@ -0,0 +1,13 @@
+package com.antonioleiva.frameworksamples.ui.screens.coroutines
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+}
