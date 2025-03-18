89d80aa731c0cc85b4c1a9973cba9ae442877155
antoniolg <antonio@devexperto.com>
2025-03-18 16:50:07 +0100

    Build Index Screen

--- app/src/main/java/com/antonioleiva/frameworksamples/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/frameworksamples/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.antonioleiva.frameworksamples
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.antonioleiva.frameworksamples.ui.components.Screen
+import com.antonioleiva.frameworksamples.ui.screens.HomeScreen
 import com.antonioleiva.frameworksamples.ui.theme.FrameworkSamplesTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val topics = listOf(
+        "2.1 Broadcast receivers",
+        "2.2 Servicios",
+        "2.3 Corrutinas",
+        "2.4 Notificaciones",
+        "2.5 Persistencia",
+        "2.6 Work Manager",
+        "2.7 Web services",
+        "2.8 Fragments",
+        "2.9 Location & Maps",
+        "2.10 Estilos y temas"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FrameworkSamplesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen(
+                    topics = topics,
+                    onTopicClick = { topic ->
+                        Toast.makeText(this, "Clicked: $topic", Toast.LENGTH_SHORT).show()
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FrameworkSamplesTheme {
-        Greeting("Android")
     }
 }
