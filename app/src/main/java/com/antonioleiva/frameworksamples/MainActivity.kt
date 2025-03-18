3ff44c3ae5bf2e9d56f7bd118c157553cda7430c
antoniolg <antonio@devexperto.com>
2025-03-18 16:58:18 +0100

    Create topics enum

--- app/src/main/java/com/antonioleiva/frameworksamples/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/frameworksamples/MainActivity.kt
@@ -5,26 +5,11 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import com.antonioleiva.frameworksamples.ui.components.Screen
+import com.antonioleiva.frameworksamples.model.Topic
 import com.antonioleiva.frameworksamples.ui.screens.HomeScreen
 import com.antonioleiva.frameworksamples.ui.theme.FrameworkSamplesTheme
 
 class MainActivity : ComponentActivity() {
-
-    private val topics = listOf(
-        "2.1 Broadcast receivers",
-        "2.2 Servicios",
-        "2.3 Corrutinas",
-        "2.4 Notificaciones",
-        "2.5 Persistencia",
-        "2.6 Work Manager",
-        "2.7 Web services",
-        "2.8 Fragments",
-        "2.9 Location & Maps",
-        "2.10 Estilos y temas"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FrameworkSamplesTheme {
                 HomeScreen(
-                    topics = topics,
+                    topics = Topic.entries,
                     onTopicClick = { topic ->
-                        Toast.makeText(this, "Clicked: $topic", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Clicked: ${getString(topic.stringRes)}", Toast.LENGTH_SHORT).show()
                     }
                 )
             }
