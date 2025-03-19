04162d9ba63a2da246297735bcb6829336d27d7a
antoniolg <antonio@devexperto.com>
2025-03-19 11:00:22 +0100

    Notifications: progress in foreground service

--- app/src/main/java/com/antonioleiva/frameworksamples/ui/screens/notifications/NotificationSamplesScreen.kt
+++ app/src/main/java/com/antonioleiva/frameworksamples/ui/screens/notifications/NotificationSamplesScreen.kt
@@ -5,6 +5,7 @@ import com.antonioleiva.frameworksamples.R
 import com.antonioleiva.frameworksamples.model.Sample
 import com.antonioleiva.frameworksamples.model.SamplesInfo
 import com.antonioleiva.frameworksamples.ui.components.SamplesScreen
+import com.antonioleiva.frameworksamples.ui.screens.services.DownloadServiceScreen
 import kotlinx.serialization.Serializable
 
 private val notificationSamples = listOf(
@@ -17,6 +18,11 @@ private val notificationSamples = listOf(
         titleRes = R.string.notification_actions,
         descriptionRes = R.string.notification_actions_description,
         destination = ActionsNotificationScreen
+    ),
+    Sample(
+        titleRes = R.string.notification_progress,
+        descriptionRes = R.string.notification_progress_description,
+        destination = DownloadServiceScreen
     )
 )
 
