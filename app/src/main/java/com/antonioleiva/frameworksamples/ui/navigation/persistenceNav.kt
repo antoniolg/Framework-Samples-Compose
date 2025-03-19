3e589a2af799576c57e2df08b6feb713a3764d5d
antoniolg <antonio@devexperto.com>
2025-03-19 11:19:19 +0100

    Persistence: Shared Preferences

---
+++ app/src/main/java/com/antonioleiva/frameworksamples/ui/navigation/persistenceNav.kt
@@ -0,0 +1,22 @@
+package com.antonioleiva.frameworksamples.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.antonioleiva.frameworksamples.ui.screens.persistence.PersistenceSamplesScreen
+import com.antonioleiva.frameworksamples.ui.screens.persistence.SharedPreferencesScreen
+
+fun NavGraphBuilder.persistenceNav(navController: NavHostController) {
+    composable<PersistenceSamplesScreen> {
+        PersistenceSamplesScreen(
+            onSampleClick = { navController.navigate(it.destination) },
+            onBack = { navController.popBackStack() }
+        )
+    }
+
+    composable<SharedPreferencesScreen> {
+        SharedPreferencesScreen(
+            onBack = { navController.popBackStack() }
+        )
+    }
+} 
